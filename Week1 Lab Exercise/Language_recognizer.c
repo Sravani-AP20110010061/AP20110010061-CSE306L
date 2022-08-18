530ba1933a6bf133a198149b2fe18d7db7c5567c
Sravani-AP20110010061 <111478644+Sravani-AP20110010061@users.noreply.github.com>
2022-08-18 11:40:42 +0530

    Update Language_recognizer.c

--- Week1 Lab Exercise/Language_recognizer.c
+++ Week1 Lab Exercise/Language_recognizer.c
@@ -1,28 +1,3 @@
-2020-A CSE 306 - Compiler Design
-Semester V : CSE - A
-Week 1 Lab ExerciseWeek 1 Lab Exercise
-Week 1 Lab Exercise
-Jaya Lakshmi Tangirala
-•
-Aug 11
-Hello all,
-CD_Lab_Week1.pdf has the instructions of lab work you need to do this week.
-Code of Program1 is available in Language_Recognizer.c
-Explanation is in the YouTube video.
-
-
-Complete the Program given in the Lab assignment. Create a repository named Week1 in your GitHub account and push your work into that, by 18th Aug 2022.
-
-Compiler Design Lab-Language Recognizer
-YouTube video   ‪20 minutes‬
-
-CD_Lab_Week1.pdf
-PDF
-
-Language_Recognizer.c
-C
-Class comments
-
 #include<stdio.h>
 void main()
 {
@@ -86,5 +61,3 @@ void main()
     else
         printf("\n\nString not accepted\n\n");
 }
-Language_Recognizer.c
-Displaying Language_Recognizer.c.
